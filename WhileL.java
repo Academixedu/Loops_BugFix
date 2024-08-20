dd88586fbf9ec7dfdf7d44f6e9a3aedbc7d1f781
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-20 11:27:33 +0530

    Revathi

--- WhileL.java
+++ WhileL.java
@@ -1,8 +1,10 @@
 public class WhileL{
-  int i=10;
-        while(i!=7){
-            System.out.println("Hello World "+i);
-            i--;
+      public static void main(String[] args) {
+      int i=10;
+      while(i!=7){
+      System.out.println("Hello World "+i);
+      i--;
   // Skip The 7 and print remaining
-        }
+}
+}
 }
