ccb7a03e2374ddad5f242248df356bc49cfce7c6
vijay767492 <vijaydandu59@gmail.com>
2024-08-20 18:26:04 +0530

    vijay

--- ForL.java
+++ ForL.java
@@ -1,12 +1,12 @@
-public class Forl{
-  public static void main(String[] args) {
-
-        for(int i=1;i<=10;i=i-2){
-            System.out.println("Hello World : "+i);
-        }
-        for(int i=15;i>15;i--){
-            System.out.println("Hello World : "+i);
-        }
-    // Correct This Errors
-    }
-}
+public class ForL{//the class name is not same file name
+    public static void main(String[] args) {
+  
+          for(int i=1;i<=10;i=i+2){//by using the decrement (i=i-2) by the  condition we get into infinite loop so changed to (i=i+2) 
+              System.out.println("Hello World : "+i);
+          }
+          for(int i=15;i>0;i--){ //i'm assuming we need to print the numbers in increment order so i replaced  i>15 with i>0
+              System.out.println("Hello World : "+i);
+          }
+      // Correct This Errors
+      }
+  }
--- WhileL.java
+++ WhileL.java
@@ -1,8 +1,20 @@
 public class WhileL{
+      public static void main(String[] args) {//main method is missing
+            
+      
   int i=10;
         while(i!=7){
             System.out.println("Hello World "+i);
             i--;
   // Skip The 7 and print remaining
-        }
+
+      }
+
+        // by using the continues method we can skip the specified number in loop 
+        // if you want to print the 10 to 0 in decount by skipping 7 
+// while(i>0){i--;
+//       if (i==7){System.out.println("bada booom bam i skiped it  :");  continue;}
+//       System.out.println("Hello World "+i);
+//       }
+}
 }
