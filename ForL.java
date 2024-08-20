11f4fc43b5104de7ed3b06694558b64b81f90f48
kurojuLucky <luckykuroju28@gmail.com>
2024-08-20 19:44:28 +0530

    l

--- ForL.java
+++ ForL.java
@@ -4,7 +4,7 @@ public class ForL{
         for(int i=1;i<=10;i=i+2){
             System.out.println("Hello World : "+i);
         }
-        for(int i=15;i>=15;i--){
+        for(int i=15;i>=1;i--){
             System.out.println("Hello World : "+i);
         }
     // Correct This Errors
