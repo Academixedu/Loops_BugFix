b062c8bf1f6053056852c31742ee1fe796214eee
Arbaz-01 <arbazshaibu@gmail.com>
2024-08-20 19:32:50 +0530

    arbaz

--- ForL.java
+++ ForL.java
@@ -1,10 +1,10 @@
-public class Forl{
+public class ForL{
   public static void main(String[] args) {
 
-        for(int i=1;i<=10;i=i-2){
+        for(int i=1;i<=10;i=i+1){
             System.out.println("Hello World : "+i);
         }
-        for(int i=15;i>15;i--){
+        for(int i=10;i>=1;i=i-1){
             System.out.println("Hello World : "+i);
         }
     // Correct This Errors
