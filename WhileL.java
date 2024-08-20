1d668a2bff27feca0d0010d5c351158c1670aeea
karthhiik <karthikthatikonda18@gmail.com>
2024-08-20 20:00:15 +0530

    Update WhileL.java

--- WhileL.java
+++ WhileL.java
@@ -1,6 +1,12 @@
 public class WhileL{
   int i=10;
-        while(i!=7){
+  
+        while(i>0)
+{
+        if(i==7){
+          i--;
+        continue;
+        }
             System.out.println("Hello World "+i);
             i--;
   // Skip The 7 and print remaining
