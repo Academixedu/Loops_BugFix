565312289d6ffd8c61eab49852ecac7c501ec5cb
kurojuLucky <luckykuroju28@gmail.com>
2024-08-20 18:58:18 +0530

    l

--- DoWhileL.java
+++ DoWhileL.java
@@ -3,10 +3,9 @@ public static void main(String[]args){
 int docount=7;
         do{
             System.out.println("Do While "+docount);
-        docount--;
+        docount=docount++;
         }
         while(docount>=15);
     }
   // Print 7 to 15 Number at doCount
-}
 }
