29d83a0c842301c50c1b6201d50d84fa2da82d5a
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-20 11:19:56 +0530

    Lavanya

--- ForL.java
+++ ForL.java
@@ -1,10 +1,12 @@
-public class Forl{
+public class ForL{
   public static void main(String[] args) {
 
-        for(int i=1;i<=10;i=i-2){
+        for(int i=1;i<=10;i=i+2){
             System.out.println("Hello World : "+i);
         }
-        for(int i=15;i>15;i--){
+
+        System.out.println("decrement------");
+        for(int i=15;i>1;i--){
             System.out.println("Hello World : "+i);
         }
     // Correct This Errors
